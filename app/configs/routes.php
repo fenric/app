84d74db15ee7f392f1b0ba35f457f14f1bff2ba3
fenric <afenric@gmail.com>
2017-12-04 01:25:44 +0300

    Незначительные правки.

--- app/configs/routes.php
+++ app/configs/routes.php
@@ -10,10 +10,8 @@ fenric('router')->get('/', \Fenric\Controllers\Home::class);
  */
 fenric('router')->scope('/admin', function($router)
 {
-	$router->get('(/)', \Fenric\Controllers\Admin\Index::class, function($router, $request, $response, $controller)
-	{
-		$controller->trailingSlashes();
-	});
+	$router->get('(/)', \Fenric\Controllers\Admin\Index::class);
+	$router->get('/update/', \Fenric\Controllers\Admin\Update::class);
 
 	$router->any('/api/<action:[a-z][a-z0-9-]*>/(<id:[1-9][0-9]{0,10}>/)', \Fenric\Controllers\Admin\Api::class);
 
@@ -47,7 +45,6 @@ fenric('router')->scope('/admin', function($router)
 	$router->any('/api/parameter/<id:[1-9][0-9]{0,10}>/<action:[a-z][a-z0-9-]*>/', \Fenric\Controllers\Admin\ApiParameter::class);
 	$router->any('/api/parameter/<action:[a-z][a-z0-9-]*>/(<id:[1-9][0-9]{0,10}>/)', \Fenric\Controllers\Admin\ApiParameter::class);
 
-	$router->get('/update/', \Fenric\Controllers\Admin\Update::class);
 });
 
 /**
@@ -55,59 +52,23 @@ fenric('router')->scope('/admin', function($router)
  */
 fenric('router')->scope('/user', function($router)
 {
-	$router->get('(/)', \Fenric\Controllers\User\Profile::class, function($router, $request, $response, $controller)
-	{
-		$controller->trailingSlashes();
-	});
+	$router->get('(/)', \Fenric\Controllers\User\Profile::class);
 
-	$router->get('/registration(/)', \Fenric\Controllers\User\Registration::class, function($router, $request, $response, $controller)
-	{
-		$controller->trailingSlashes();
-	});
-	$router->post('/registration/process(/)', \Fenric\Controllers\User\RegistrationProcess::class, function($router, $request, $response, $controller)
-	{
-		$controller->trailingSlashes();
-	});
-	$router->get('/registration/<code:[a-z0-9]{40}>(/)', \Fenric\Controllers\User\RegistrationConfirm::class, function($router, $request, $response, $controller)
-	{
-		$controller->trailingSlashes();
-	});
+	$router->get('/registration(/)', \Fenric\Controllers\User\Registration::class);
+	$router->post('/registration/process(/)', \Fenric\Controllers\User\RegistrationProcess::class);
+	$router->get('/registration/<code:[a-z0-9]{40}>(/)', \Fenric\Controllers\User\RegistrationConfirm::class);
 
-	$router->get('/login(/)', \Fenric\Controllers\User\Authentication::class, function($router, $request, $response, $controller)
-	{
-		$controller->trailingSlashes();
-	});
-	$router->post('/login/process(/)', \Fenric\Controllers\User\AuthenticationProcess::class, function($router, $request, $response, $controller)
-	{
-		$controller->trailingSlashes();
-	});
-	$router->get('/login/<code:[a-z0-9]{40}>(/)', \Fenric\Controllers\User\AuthenticationByToken::class, function($router, $request, $response, $controller)
-	{
-		$controller->trailingSlashes();
-	});
+	$router->get('/login(/)', \Fenric\Controllers\User\Authentication::class);
+	$router->post('/login/process(/)', \Fenric\Controllers\User\AuthenticationProcess::class);
+	$router->get('/login/<code:[a-z0-9]{40}>(/)', \Fenric\Controllers\User\AuthenticationByToken::class);
 
-	$router->get('/login/token/create(/)', \Fenric\Controllers\User\AuthenticationTokenCreate::class, function($router, $request, $response, $controller)
-	{
-		$controller->trailingSlashes();
-	});
-	$router->post('/login/token/create/process(/)', \Fenric\Controllers\User\AuthenticationTokenCreateProcess::class, function($router, $request, $response, $controller)
-	{
-		$controller->trailingSlashes();
-	});
+	$router->get('/login/token/create(/)', \Fenric\Controllers\User\AuthenticationTokenCreate::class);
+	$router->post('/login/token/create/process(/)', \Fenric\Controllers\User\AuthenticationTokenCreateProcess::class);
 
-	$router->get('/logout(/)', \Fenric\Controllers\User\SignOut::class, function($router, $request, $response, $controller)
-	{
-		$controller->trailingSlashes();
-	});
+	$router->get('/logout(/)', \Fenric\Controllers\User\SignOut::class);
 
-	$router->get('/settings(/)', \Fenric\Controllers\User\ProfileSettings::class, function($router, $request, $response, $controller)
-	{
-		$controller->trailingSlashes();
-	});
-	$router->post('/settings/save(/)', \Fenric\Controllers\User\ProfileSettingsSave::class, function($router, $request, $response, $controller)
-	{
-		$controller->trailingSlashes();
-	});
+	$router->get('/settings(/)', \Fenric\Controllers\User\ProfileSettings::class);
+	$router->post('/settings/save(/)', \Fenric\Controllers\User\ProfileSettingsSave::class);
 
 	$router->any('/api/<action:[a-z][a-z0-9-]*>/(<id:[1-9][0-9]{0,10}>/)', \Fenric\Controllers\User\Api::class);
 });
@@ -130,58 +91,37 @@ fenric('router')->scope('/api', function($router)
 /**
  * Регистрация клика по баннеру
  */
-fenric('router')->get('/click/<id:[1-9][0-9]{0,10}>(/)', \Fenric\Controllers\Banner::class, function($router, $request, $response, $controller)
-{
-	$controller->trailingSlashes();
-});
+fenric('router')->get('/click/<id:[1-9][0-9]{0,10}>(/)', \Fenric\Controllers\Banner::class);
 
 /**
  * Поиск по сайту
  */
-fenric('router')->get('/search(/)', \Fenric\Controllers\Search::class, function($router, $request, $response, $controller)
-{
-	$controller->trailingSlashes();
-});
+fenric('router')->get('/search(/)', \Fenric\Controllers\Search::class);
 
 /**
  * Список тегов
  */
-fenric('router')->get('/tags(/)', \Fenric\Controllers\Tags::class, function($router, $request, $response, $controller)
-{
-	$controller->trailingSlashes();
-});
+fenric('router')->get('/tags(/)', \Fenric\Controllers\Tags::class);
 
 /**
  * Список публикаций тега
  */
-fenric('router')->get('/tags/<code:[a-z0-9-]+>(/)', \Fenric\Controllers\Tag::class, function($router, $request, $response, $controller)
-{
-	$controller->trailingSlashes();
-});
+fenric('router')->get('/tags/<code:[a-z0-9-]+>(/)', \Fenric\Controllers\Tag::class);
 
 /**
  * Список пользователей
  */
-fenric('router')->get('/users(/)', \Fenric\Controllers\Users::class, function($router, $request, $response, $controller)
-{
-	$controller->trailingSlashes();
-});
+fenric('router')->get('/users(/)', \Fenric\Controllers\Users::class);
 
 /**
  * Страница пользователя
  */
-fenric('router')->get('/users/<id:[1-9][0-9]{0,10}>(/)', \Fenric\Controllers\User::class, function($router, $request, $response, $controller)
-{
-	$controller->trailingSlashes();
-});
+fenric('router')->get('/users/<id:[1-9][0-9]{0,10}>(/)', \Fenric\Controllers\User::class);
 
 /**
  * Страница раздела
  */
-fenric('router')->get('/<section:[a-z0-9-]{1,255}>(/<tag:[a-z0-9-]{1,255}>)(/)', \Fenric\Controllers\Section::class, function($router, $request, $response, $controller)
-{
-	$controller->trailingSlashes();
-});
+fenric('router')->get('/<section:[a-z0-9-]{1,255}>(/<tag:[a-z0-9-]{1,255}>)(/)', \Fenric\Controllers\Section::class);
 
 /**
  * Страница публикации
@@ -189,12 +129,46 @@ fenric('router')->get('/<section:[a-z0-9-]{1,255}>(/<tag:[a-z0-9-]{1,255}>)(/)',
 fenric('router')->get('/<section:[a-z0-9-]{1,255}>/<publication:[a-z0-9-]{1,255}>.html', \Fenric\Controllers\Publication::class);
 
 /**
- * Сервисы
+ * Sitemap.xml
  */
-fenric('router')->get('/robots.txt', \Fenric\Controllers\Services\RobotsTxt::class);
-fenric('router')->get('/sitemap.xml', \Fenric\Controllers\Services\SitemapXml::class);
+fenric('router')->get('/sitemap.xml', \Fenric\Controllers\Sitemap::class);
 
 /**
- * JS файл с системными данными
+ * favicon.ico
  */
-fenric('router')->any('/assets/fenric.js', \Fenric\Controllers\Assets\FenricJs::class);
+fenric('router')->get('/favicon.ico', \Fenric\Controllers\Blank::class, function($router, $request, $response, $controller)
+{
+	$response->setStatus(404);
+	$response->setHeader('Content-type: image/x-icon');
+	$response->setContent('');
+});
+
+/**
+ * humans.txt
+ */
+fenric('router')->get('/humans.txt', \Fenric\Controllers\Blank::class, function($router, $request, $response, $controller)
+{
+	$response->setStatus(200);
+	$response->setHeader('Content-type: text/plain; charset=UTF-8');
+	$response->setContent(fenric('parameter::humans.txt') ?: '');
+});
+
+/**
+ * robots.txt
+ */
+fenric('router')->get('/robots.txt', \Fenric\Controllers\Blank::class, function($router, $request, $response, $controller)
+{
+	$response->setStatus(200);
+	$response->setHeader('Content-type: text/plain; charset=UTF-8');
+	$response->setContent(fenric('parameter::robots.txt') ?: '');
+});
+
+/**
+ * JavaScript файл с системными данными
+ */
+fenric('router')->any('/assets/fenric.js', \Fenric\Controllers\Blank::class, function($router, $request, $response, $controller)
+{
+	$response->setStatus(200);
+	$response->setHeader('Content-Type: application/javascript; charset=UTF-8');
+	$response->setContent(fenric('view::assets/fenric.js')->render());
+});
